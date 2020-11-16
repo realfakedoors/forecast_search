0fa55ccd6a6f716c9ba4f42fa90caf1947601384
Andy Holt <andyholt85@gmail.com>
2020-11-16 11:37:47 -0700

    set up initial project structure

---
+++ src/components/Forecast.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Forecast = () => {
+  return (
+    <div className="Forecast">
+    
+    </div>
+  );
+}
+
+export default Forecast;
---
+++ src/components/Location.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Location = () => {
+  return (
+    <div className="Location">
+    
+    </div>
+  );
+}
+
+export default Location;
